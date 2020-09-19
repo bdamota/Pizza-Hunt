0cdcae3ca4434d4753d882fe9778efef6aea6f8b
LYNN DAMOTA <bdamota0506@gmail.com>
2020-09-18 21:13:35 -0700

    user comment model complete

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,31 +1,34 @@
 const { Pizza } = require('../models');
 
 const pizzaController = {
-    // get all pizzas
-    getAllPizza(req, res) {
-      Pizza.find({})
-        .then(dbPizzaData => res.json(dbPizzaData))
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+  getAllPizza(req, res) {
+    Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
     },
   
     // get one pizza by id
-    getPizzaById({ params }, res) {
-      Pizza.findOne({ _id: params.id })
-        .then(dbPizzaData => {
-          // If no pizza is found, send 404
-          if (!dbPizzaData) {
-            res.status(404).json({ message: 'No pizza found with this id!' });
-            return;
-          }
-          res.json(dbPizzaData)
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
+getPizzaById({ params }, res) {
+  Pizza.findOne({ _id: params.id })
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbPizzaData => res.json(dbPizzaData))
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(400);
+    });
     },
     // createPizza
     createPizza({ body }, res) {
@@ -56,7 +59,15 @@ const pizzaController = {
         res.json(dbPizzaData);
       })
       .catch(err => res.status(400).json(err));
+  },
   }
-  }
+
+  Pizza.find({})
+  .populate({
+    path: 'comments',
+    select: '-__v'
+  })
+  .select('-__v')
+  .then(dbPizzaData => res.json(dbPizzaData))
 
 module.exports = pizzaController;
